9ba9bb0093d41213b832b68c0421d430075551a6
pramodsaini <pramodsaini25062000@gmail.com>
2024-04-04 22:14:58 -0700

    Read,dynamic,insert,json_encode and json_decode

--- insertJson.php
+++ insertJson.php
@@ -8,27 +8,72 @@
 </head>
 <body>
      
-<div id="demo"></div> 
+ 
 Roll.No:<input type="text" id="roll"><br><br>
 Name:<input type="text" id="name"><br><br>
 Avgr:<input type="text" id="avg"><br><br>
-<button class="submit">Submit</button>
+<button class="insert">Insert</button>&nbsp;&nbsp;<button class="read">Read Json </button> &nbsp;&nbsp;<button class="encode">Encode</button>&nbsp;&nbsp;<button class="decode">Decode</button>
 <script>
      $(document).ready(function(){
-        $(".submit").click(function(){
+        $(".insert").click(function(){
             var roll=$("#roll").val();
             var name=$("#name").val();
             var avg=$("#avg").val();
-            $.ajax({
+            if(roll==""||name==""||avg==""){
+                alert("All Field Are Required");
+            }
+            else{
+                $.ajax({
                 url:"insert_data_json.php",
                 type:"POST",
                 data:{roll:roll,name:name,avg:avg},
                 success:function(data){
+                    $("#demo").html("");
                     $("#demo").html(data);
                 }
             });
+            }
         });
+        $(".read").click(function(){
+        //    $.getJSON(
+        //       "json/data.json",
+        //       function(data){
+        //         $.each(data,function(key,value){
+        //             $("#demo").append(value.sn+" "+value.name+" "+value.avgr+"<br>");
+        //         });
+        //       }
+        //    )
+           $.ajax({
+             url:"json/data.json",
+             type:"GET",
+             dataType:"JSON",
+             success:function(data){
+                $.each(data,function(key,value){
+                    $("#demo").append(value.sn+" "+value.name+" "+value.avgr+"<br>");
+                })
+             }
+           });
+        });
+        $(".encode").click(function(){
+            $.ajax({
+                url:"encode_fetch_data.php",
+                type:"POST",
+                success:function(data){
+                     $("#demo").html(data);
+                }
+            })
+        })
+        $(".decode").click(function(){
+            $.ajax({
+                url:"decode_fetch_data.php",
+                type:"POST",
+                success:function(data){
+                     $("#demo").append(data);
+                }
+            })
+        })
      });
 </script>
+<div id="demo"></div> 
 </body>
 </html>
